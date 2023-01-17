43e720a9169a0c29fd26fb831b5d1f190cba9678
fishiversity <41026299+fishiversity@users.noreply.github.com>
2023-01-17 11:57:30 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
 input.onButtonPressed(Button.A, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1925, 0, 255, 0, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
     basic.showIcon(IconNames.Heart)
 })
 input.onButtonPressed(Button.B, function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sine, 1, 1960, 255, 0, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
     basic.showIcon(IconNames.SmallHeart)
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.ringTone(50)
     basic.showLeds(`
         # . . . .
         # . . . .
@@ -40,5 +43,7 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         # # # # #
         # # # # #
         `)
+    music.stopAllSounds()
+    basic.clearScreen()
 })
 basic.showIcon(IconNames.Happy)
